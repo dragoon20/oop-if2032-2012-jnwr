9623c0c0a799da16031d2441db440b764505247d
fernandojordan.92 <fernandojordan.92@gmail.com>
2012-03-05 03:50:51 +0000

    membenarkan nama file

--- src/Driver/mPoint.cpp
+++ src/Driver/mPoint.cpp
@@ -11,59 +11,59 @@ int main () {
 	P1.SetX(0) ;
 	P1.SetY(0) ;
 	cout << "Dibentuk sebuah point P1" << endl ;
-	// cout << "P1 = " << P1 << endl ;
-	// cout << endl ; 
+	cout << "P1 = " << P1 << endl ;
+	cout << endl ; 
 	
-	// cout << "Dibentuk sebuah point P2" << endl ;
-	// Point P2(1,1) ;
-	// cout << "P2 = " << P2 << endl ;
-	// cout << endl ;
+	cout << "Dibentuk sebuah point P2" << endl ;
+	Point P2(1,1) ;
+	cout << "P2 = " << P2 << endl ;
+	cout << endl ;
 	
-	// cout << "Dibentuk sebuah point P3 = P2" << endl ;
-	// Point P3 = P2 ;
-	// cout << "P3 = " << P3 << endl ;
-	// cout << endl ;
+	cout << "Dibentuk sebuah point P3 = P2" << endl ;
+	Point P3 = P2 ;
+	cout << "P3 = " << P3 << endl ;
+	cout << endl ;
 	
-	// cout << "Apakah titik P1 merupakan titik origin?" << endl ;
-	// if (IsOrigin(P1))
-		// cout << "Ya" << endl ;
-	// else
-		// cout << "Tidak" << endl ;
-	// cout << endl ;
+	cout << "Apakah titik P1 merupakan titik origin?" << endl ;
+	if (IsOrigin(P1))
+		cout << "Ya" << endl ;
+	else
+		cout << "Tidak" << endl ;
+	cout << endl ;
 	
-	// cout << "Apakah titik P2 merupakan titik origin?" << endl ;
-	// if (IsOrigin(P2))
-		// cout << "Ya" << endl ;
-	// else
-		// cout << "Tidak" << endl ;
-	// cout << endl ;
+	cout << "Apakah titik P2 merupakan titik origin?" << endl ;
+	if (IsOrigin(P2))
+		cout << "Ya" << endl ;
+	else
+		cout << "Tidak" << endl ;
+	cout << endl ;
 	
-	// cout << "P3 dipindahkan sebanyak 3 ke arah sumbu X dan 1 ke arah sumbu Y" << endl ;
-	// P3.move(3,1) ;
-	// cout << "P3 = " << P3 << endl ;
-	// cout << endl ;
+	cout << "P3 dipindahkan sebanyak 3 ke arah sumbu X dan 1 ke arah sumbu Y" << endl ;
+	P3.move(3,1) ;
+	cout << "P3 = " << P3 << endl ;
+	cout << endl ;
 	
-	// cout << "P3 dicerminkan terhadap titik P2" << endl ;
-	// P3.mirror(P2) ;
-	// cout << "P3 = " << P3 << endl ;
-	// cout << endl ;
+	cout << "P3 dicerminkan terhadap titik P2" << endl ;
+	P3.mirror(P2) ;
+	cout << "P3 = " << P3 << endl ;
+	cout << endl ;
 	
-	// cout << "P3 diputar 90 derajat terhadap titik P2" << endl ;
-	// P3.rotate(P2, 1) ;
-	// cout << "P3 = " << P3 << endl ;
-	// cout << endl ;
+	cout << "P3 diputar 90 derajat terhadap titik P2" << endl ;
+	P3.rotate(P2, 1) ;
+	cout << "P3 = " << P3 << endl ;
+	cout << endl ;
 	
-	// cout << "P3 diputar 270 derajat terhadap titik P2" << endl ;
-	// P3.rotate(P2, 3) ;
-	// cout << "P3 = " << P3 << endl ;
-	// cout << endl ;
+	cout << "P3 diputar 270 derajat terhadap titik P2" << endl ;
+	P3.rotate(P2, 3) ;
+	cout << "P3 = " << P3 << endl ;
+	cout << endl ;
 	
-	// cout << "P3 diputar 180 derajat terhadap titik P2" << endl ;
-	// P3.rotate(P2, 2) ;
-	// cout << "P3 = " << P3 << endl ;
-	// cout << endl ;
+	cout << "P3 diputar 180 derajat terhadap titik P2" << endl ;
+	P3.rotate(P2, 2) ;
+	cout << "P3 = " << P3 << endl ;
+	cout << endl ;
 	
-	// cout << "Kuadran dari P3 adalah : " << P3.kuadran() << endl ;
-	// cout << "Kuadran dari P2 adalah : " << P2.kuadran() << endl ;
-	// cout << "Kuadran dari P1 adalah : " << P1.kuadran() << endl ;
+	cout << "Kuadran dari P3 adalah : " << P3.kuadran() << endl ;
+	cout << "Kuadran dari P2 adalah : " << P2.kuadran() << endl ;
+	cout << "Kuadran dari P1 adalah : " << P1.kuadran() << endl ;
 }
