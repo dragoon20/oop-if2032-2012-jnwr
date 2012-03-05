4eb538efe6439fae5cc80c5df5eefa4cc7f0d49d
wilsonfonda@gmail.com <wilsonfonda@gmail.com@2b3e72a5-9500-180b-d71d-048d253a1109>
2012-03-05 04:44:32 +0000

    sel.cpp digabung ke sel.h  tambah driver sel
    sel.h masih rusak, ada kesalahan yg gw gk ngerti di bagaian template

--- trunk/src/Sel/Sel.h
+++ trunk/src/Sel/Sel.h
@@ -1,23 +1,49 @@
 #ifndef SEL_H
 #define SEL_H
-#include "../Point/point.h
+#include "../Point/point.h"
 template <class tes>
-class sel<tes>:public point
+
+class sel<tes> : public point
 {
     public:
         sel();
         sel(point, tes);
         virtual ~sel();
-        sel(const sel&);
-        sel& operator=(const sel&);
+        sel(const sel&)
+{
+    //copy ctor
+    x = other.x;
+    y = other.y;
+    t = other.t;
+}
+        sel& operator=(const sel&){ x = sel.x;
+                                    y = sel.y;
+                                    tes = sel.tes;}
         //Getter
-        point getp();
-        tes gett();
+        point getp(){
+    point P;
+    P.x=x;
+    P.y=y;
+    return P;
+}
+        tes gett()
+{
+    tes T;
+    T=t;
+    return T;
+}
 
         //setter
         void setp(const point& p);
-        void sett(const tes& t);
+{
+    x=P.x;
+    y=P.y;
+}
 
+        void sett(const tes& t);
+{
+    t=T;
+}
 
     private:
         tes t;
