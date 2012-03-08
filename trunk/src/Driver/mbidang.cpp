1bf5d8a2637a455752fbec6cc74e223e06fb5585
wilsonfonda@gmail.com <wilsonfonda@gmail.com@2b3e72a5-9500-180b-d71d-048d253a1109>
2012-03-08 15:59:56 +0000

    mbidang ud dbenerin, mmatrix ditambahin

--- trunk/src/Driver/mbidang.cpp
+++ trunk/src/Driver/mbidang.cpp
@@ -1,4 +1,4 @@
-//File : mlatar.cpp
+//File : mbidang.cpp
 
 #include "../Bidang/bidang.h"
 #include <iostream>
@@ -6,24 +6,18 @@
 using namespace std;
 int main()
 {
-	bidang<char> bidtes(15,15,3,'#');
-	cout<<"ukuran bidang ="<<bidtes.getukuran()<<endl;
-	cout<<"ganti ukuran menjadi 4"<<endl;
-	bidtes.setukuran(4);
+	bidang<char> bidtes(15,15);
+	cout<<"segi bidang ="<<bidtes.getsegi()<<endl;
+	cout<<"ganti segi menjadi 4"<<endl;
+	bidtes.setsegi(4);
 	cout<<bidtes<<endl;
 
 	cout<<"method move"<<endl;
 	bidtes.move (1,1);
 	cout<<bidtes<<endl;
 
-	point P(5,5);
-	cout<<"method mirror"<<endl;
-	bidtes.mirror(P);
-	cout<<bidtes<<endl;
-
 	cout<<"method rotate"<<endl;
-	bidtes.rotate(P,3);
+	bidtes.rotate(3);
 	cout<<bidtes<<endl;
-
 
 }
---
+++ trunk/src/Driver/mmatrix.cpp
@@ -0,0 +1,9 @@
+//file : mmatrix.cpp
+
+#include "../Matrix/matrix.h"
+
+int main ()
+{
+    cout<<"buat matrix 5x5"<<endl;
+    matrix(5,5,'&');
+}
